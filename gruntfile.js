4cac651ae64b2f877d7a6b67be8000b889a1c4fe
selcher <selcher123@gmail.com>
2015-06-30 22:57:33 +0800

    Add grunt for uglify and cssmin tasks. Revert changes on css media ( not working )

---
+++ gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function( grunt ) {
+
+	// Project Configuration
+	grunt.initConfig( {
+		pkg : grunt.file.readJSON( 'package.json' ),
+		buildSrcFile : 'js/init.js',
+		buildSrcMinFile : 'js/init.min.js',
+		buildCssSrcFile : 'css/style.css',
+		buildCssSrcMinFile : 'css/style.min.css',
+		uglify : {
+			options : {
+				banner : '/**\n' +
+  						' * <%= pkg.name %> - v<%= pkg.version %> - build <%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %>\n' +
+  						' * Copyright (c) 2015 Selcher;\n' +
+  						' * Distributed under the terms of the ISC license.\n' +
+						' */\n'
+			},
+			build : {
+				src : '<%= buildSrcFile %>',
+				dest : '<%= buildSrcMinFile %>'
+			}
+		},
+		cssmin : {
+			css : {
+				src : '<%= buildCssSrcFile %>',
+				dest : '<%= buildCssSrcMinFile %>'
+			}
+		},
+		watch : {
+			files: [ '<%= buildSrcFile %>', '<%= buildCssSrcFile %>' ],
+			tasks: [ 'cssmin', 'uglify' ]
+		}
+	} );
+
+	// Load the plugins
+	// uglify -> minify
+	// grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+
+	// Replace multiple loadNpmTasks with matchdep and let it automatically add them
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+
+	// load Npm tasks... alertnative to matchdep
+	// require( 'load-grunt-tasks' )( grunt, [ 'grunt-*', '!grunt-template-jasmine-requirejs' ] );
+
+	// Defalt task : just minify code
+	grunt.registerTask( 'default', [ 'cssmin', 'uglify' ] );
+
+	// Development : watch for file changes and run:
+	// 1. jasmine for code testing
+	grunt.registerTask( 'dev', [ 'watch' ] );
+
+	// Production : check code and then minify
+	grunt.registerTask( 'prod', [ 'cssmin', 'uglify' ] );
+
+};
